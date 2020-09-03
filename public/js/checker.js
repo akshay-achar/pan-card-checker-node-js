ca1837106b4ef5e5bc976c58dd52dda4087b3b44
Akshay Karkala <akshay.k@techjini.com>
2020-09-03 23:32:52 +0530

    added the loading screen and style

--- public/js/checker.js
+++ public/js/checker.js
@@ -12,10 +12,11 @@ var loadFile = function(event) {
 }
 
 $("#panCardValidationForm").on('submit', (e) => {
+  $.LoadingOverlay("show")
   e.preventDefault()
   document.getElementById('message').style.display = 'none'
-  if ($('#file')[0].files[0].type.includes('image')) {  
-    var fd = new FormData();
+  if ($('#file')[0].files[0].type.includes('image')) {
+    var fd = new FormData()
     fd.append('fileUpload', $('#file')[0].files[0])
     $.ajax({
       type: "POST",
@@ -27,6 +28,7 @@ $("#panCardValidationForm").on('submit', (e) => {
       success: function (result) {
         $('#message').text('Result : ' + result)
         document.getElementById('message').style.display = ''
+        $.LoadingOverlay('hide')
     }
     });
   } else {
